bbf459384198a8bb540e1367feea98e93d21b790
CredibleOpossum <CredibleOpossum@protonmail.com>
2025-04-21 06:10:26 -0400

    this should be fixed finally

--- initalize_and_update_submodules.sh
+++ initalize_and_update_submodules.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule update --remote --merge
-git submodule update --init --recursive
-
+git submodule update --recursive --remote
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
