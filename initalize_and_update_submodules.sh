6d35c883652d73c435869075e6c990cbba35bd62
CredibleOpossum <CredibleOpossum@protonmail.com>
2025-06-14 20:16:44 -0400

    ugh

--- initalize_and_update_submodules.sh
+++ initalize_and_update_submodules.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule update --recursive --remote
+git submodule update --init --recursive --remote
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
