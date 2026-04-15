1ec464f29adae24754ee47a93ff996b2de574740
Zander <CredibleOpossum@protonmail.com>
2026-04-15 15:51:07 -0400

    update deps, use new wow

--- build-wine.sh
+++ build-wine.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-docker build . -t wine-builder -f docker/Dockerfile && \
-export container_id=$(docker run -v $(pwd)/wine:/app/wine/:z --user=$(id -u):$(id -g) -it --detach wine-builder) && \
+set -e
+
+docker build . -f ./docker/Dockerfile -t wine-builder
+
+container_id=$(docker run -v $(pwd)/wine:/app/wine/:z --user=$(id -u):$(id -g) -it --detach wine-builder) && \
+
 docker logs -f $container_id && \
-docker cp $container_id:/app/wine_build/wine-master . && \
+docker cp $container_id:/app/wine_build/wine_master . && \
 docker rm $container_id
